eacaa5060858b7f64179ccad012ff8a557418e55
Dudi1896 <denzelu181@gmail.com>
2023-05-13 01:04:25 -0400

    misc

--- app/layout.jsx
+++ app/layout.jsx
@@ -7,18 +7,18 @@ export const metadata = {
   title: 'Promptopia',
   description: 'Discover & Share AI Prompts',
 }
- 
-const RootLayout = ({ children })  => {
- return (
+
+const RootLayout = ({ children }) => {
+  return (
     <html lang='en'>
       <body>
-        
-      <div className='main'>
-          <div className='gradient' />
-        </div>
+        <Provider>
+          <div className='main'>
+            <div className='gradient' />
+          </div>
 
-
-      <main className='app'><Nav/>{children}</main>
+          <main className='app'><Nav />{children}</main>
+        </Provider>
       </body>
     </html>
   )
